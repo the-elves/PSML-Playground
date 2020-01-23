2af417b939155a80593bebf32eeaab18b81f1405
aj <the-elves@bitbucket.org>
2020-01-23 09:28:58 +0530

    minimized program

--- EnumerativeSearchSynthesis/top-down-enumerative-search.py
+++ EnumerativeSearchSynthesis/top-down-enumerative-search.py
@@ -26,12 +26,6 @@ class CFG(object):
 
     
     
-    def is_nonterminal(self, symbol):
-        if(symbol in self.prod[symbol]):
-           return True
-        else:
-           return False
-
     def prod_contains_nonterminal(self, prod):
         for sym in prod:
             if sym in self.prod:
@@ -40,65 +34,10 @@ class CFG(object):
     
         
 
-    def gen_sentence(self, Symbol,depth):
-        global DEBUG
-        global TREE_DEPTH
-        if DEBUG:
-            print("==== depth = "+str(depth) + "symbol = " + Symbol)
-        """ Generate a random sentence from the
-            grammar, starting with the given
-            symbol.
-        """
-        # make a list of viable productions according to length
-        if(depth <= 0):
-#            print("negative depth reached")
-#            if depth exhausted only make productions to terminals
-            next_prods  = [ p for p in self.prod[Symbol] if not self.prod_contains_nonterminal(p)]
-        else:
-            next_prods = self.prod[Symbol]
-        next_prods.sort(key=len)
-        
-        los = []
-#        print("next_prods = {}".format(next_prods))
-        for prod in next_prods:
-            if DEBUG:
-                print("Prod = {}".format(prod))
-            sentence  = ['']
-            for sym in prod:
-                if DEBUG:
-                    print("symbol = {}".format(sym))
-            # for non-terminals, recurse
-                if sym in self.prod:
-                    _sentence = []
-                    if sym in self.subs:
-                        subs = self.subs[sym]
-                    else:
-                        depth-=1
-                        subs = self.gen_sentence(sym,depth)
-                        depth+=1
-                    if DEBUG:
-                        print("subs = {}".format(subs))
-                    for s in sentence:
-                        for sub in subs:
-                            _sentence.append(s+sub)
-                    sentence = _sentence
-                        
-                else:
-                    sentence = [s + sym + ' ' for s in sentence]
-                if DEBUG:
-                    print("sentence = {}".format(sentence))
-            los = los + sentence
-            if DEBUG:
-                print ("partial sentence = {}".format(sentence))
-            if DEBUG:
-                print("los = {} sentences added = {}".format(los, sentence))
-        if(depth == TREE_DEPTH):
-            self.subs[Symbol] = los
-        return los
+
 
     def get_next_prog(self):
         top = self.q.pop(0)
-#        print ("top = {}".format(top))
         while self.prod_contains_nonterminal(top):
             i = 0
             while ( top[i] not in self.prod ):
